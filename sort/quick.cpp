1704cbbfeed41f46217cee0ca5f31c2cccbf5f91
miyata-h256 <mhprivate79@gmail.com>
2022-10-26 06:46:55 +0900

    quick

---
+++ sort/quick.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void QuickSort(vector<int> &a,int left,int right){
+    if(right - left <= 1) return;
+
+    int pivot_index = (left+right)/2;
+    int pivot = a[pivot_index];
+    swap(a[pivot_index],a[right-1]);
+
+    int i=left;
+    for(int j=left;j<right -1;j++){
+        //pivot未満のものがあれば左に詰めていく
+        if(a[j]<pivot){
+            swap(a[i++],a[j]);
+        }
+    }
+    swap(a[i],a[right-1]);//pivotを適切な場所に挿入
+
+    for(int i=0;i<right;i++){
+        printf("%5d",a[i]);
+    }
+    putchar('\n');
+
+    //再帰的に解く
+    QuickSort(a,left,i);//pivot未満
+    QuickSort(a,i+1,right);//pivot以上
+}
+
+int main(){
+    int N;
+    cin >> N;
+    vector<int> a(N);
+    for(int i=0;i<N;i++){
+        cin >> a[i];
+    }
+
+    QuickSort(a,0,N);
+
+    for(int i=0;i<N;i++){
+        printf("%5d",a[i]);
+    }
+    putchar('\n');
+}
